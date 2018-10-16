04a76c29f12bf537c3e8ae66be09990b2573c628
James Nocentini <james.nocentini@gmail.com>
2018-10-16 18:10:09 +0200

    update package.sh

--- modules/ROOT/examples/package.sh
+++ modules/ROOT/examples/package.sh
@@ -4,25 +4,26 @@ OUTPUT_FILE=${1}
 if [[ ! -z ${1} ]]; then
 		echo "Temporary folder..."
 		mkdir $OUTPUT_FILE
-		cp -R HotelFinder "${OUTPUT_FILE}/"
+		cp -R HotelLister "${OUTPUT_FILE}/"
+		cp -R cordova-plugin-hotel-lister "${OUTPUT_FILE}/"
 		echo "Clean... ${1}"
 
 		if [ "${1}" = "starter-project" ]; then
-			rm -rf "${OUTPUT_FILE}/HotelFinder/ios/HotelFinder/travel-sample.cblite2"
-			rm -rf "${OUTPUT_FILE}/HotelFinder/android/app/src/main/assets/travel-sample.cblite2.zip"
+			rm -rf "${OUTPUT_FILE}/HotelLister/platforms/android/app/src/main/assets/travel-sample.cblite2.zip"
+			rm -rf "${OUTPUT_FILE}/cordova-plugin-hotel-lister/src/android/DatabaseManager.java"
+			rm -rf "${OUTPUT_FILE}/cordova-plugin-hotel-lister/src/android/HotelLister.java"
 		elif [ "${1}" = "final-project" ]; then
 			echo ""
 		else
 			echo "Error: the output file must be called starter-project or final-project"
 		fi
 
-		rm -rf "${OUTPUT_FILE}/HotelFinder/node_modules/"
-		rm -rf "${OUTPUT_FILE}/HotelFinder/ios/Frameworks/CouchbaseLiteSwift.framework"
-		rm -rf "${OUTPUT_FILE}/HotelFinder/android/app/build"
-		rm -rf "${OUTPUT_FILE}/HotelFinder/android/.idea"
-		rm -rf "${OUTPUT_FILE}/HotelFinder/android/.gradle"
-		rm -rf "${OUTPUT_FILE}/HotelFinder/android/local.properties"
-		rm -rf "${OUTPUT_FILE}/HotelFinder/android/HotelFinder.iml"
+		rm -rf "${OUTPUT_FILE}/HotelLister/node_modules/"
+		rm -rf "${OUTPUT_FILE}/HotelLister/platforms/android/app/build"
+		rm -rf "${OUTPUT_FILE}/HotelFinder/platforms/android/.idea"
+		rm -rf "${OUTPUT_FILE}/HotelFinder/platforms/android/.gradle"
+		rm -rf "${OUTPUT_FILE}/HotelFinder/platforms/android/local.properties"
+		rm -rf "${OUTPUT_FILE}/HotelFinder/platforms/android/HotelFinder.iml"
 
 		echo "Zipping..."
 		ditto -c -k --sequesterRsrc --keepParent "${OUTPUT_FILE}" "${OUTPUT_FILE}.zip"
