bb80fd676bf9035b3a9eee8f10c1368a52c2712f
James Nocentini <james.nocentini@gmail.com>
2018-10-16 02:27:56 +0200

    update UI

--- modules/ROOT/examples/HotelLister/src/pages/home/home.ts
+++ modules/ROOT/examples/HotelLister/src/pages/home/home.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+declare var window: any;
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  hotels: any[];
 
   constructor(public navCtrl: NavController) {
+
+    // tag::query-hotels[]
+    window.plugins.HotelLister.queryHotels((hotels) => {
+      console.log(hotels);
+      this.hotels = hotels;
+    });
+    // end::query-hotels[]
 
   }
 
