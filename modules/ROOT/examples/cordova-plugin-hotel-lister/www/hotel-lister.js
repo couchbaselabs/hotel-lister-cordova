415058ad84fa903f64c86428429bd2ed6f5ee1b4
James Nocentini <james.nocentini@gmail.com>
2018-10-16 22:31:31 +0200

    add tags

--- modules/ROOT/examples/cordova-plugin-hotel-lister/www/hotel-lister.js
+++ modules/ROOT/examples/cordova-plugin-hotel-lister/www/hotel-lister.js
@@ -1,9 +1,11 @@
 var exec = require('cordova/exec');
 
 var hotelLister = {
+  // tag::query-hotels[]
   queryHotels: function(successCallback, errorCallback) {
     exec(successCallback, errorCallback, 'HotelLister', 'queryHotels');
   },
+  // end::query-hotels[]
 };
 
 module.exports = hotelLister;
