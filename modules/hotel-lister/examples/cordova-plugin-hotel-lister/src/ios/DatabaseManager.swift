dada68ba26ba50ffd708419e68d93ecb0e13eddd
James Nocentini <james.nocentini@gmail.com>
2018-10-24 13:42:30 +0200

    add DatabaseManager.swift and reference it in plugin.xml

---
+++ modules/hotel-lister/examples/cordova-plugin-hotel-lister/src/ios/DatabaseManager.swift
@@ -0,0 +1,46 @@
+// tag::setup-database[]
+import CouchbaseLiteSwift
+
+class DatabaseManager {
+
+    private static var privateSharedInstance: DatabaseManager?
+
+    var database: Database
+
+    let DB_NAME = "travel-sample"
+
+    class func sharedInstance() -> DatabaseManager   {
+        guard let privateInstance = DatabaseManager.privateSharedInstance else {
+            DatabaseManager.privateSharedInstance = DatabaseManager()
+            return DatabaseManager.privateSharedInstance!
+        }
+        return privateInstance
+    }
+
+    private init() {
+        let path = Bundle.main.path(forResource: self.DB_NAME, ofType: "cblite2")!
+        if !Database.exists(withName: self.DB_NAME) {
+            do {
+                try Database.copy(fromPath: path, toDatabase: self.DB_NAME, withConfig: nil)
+            } catch {
+                fatalError("Could not copy database")
+            }
+        }
+        do {
+            self.database = try Database(name: "travel-sample")
+            self.createIndex(database)
+        } catch {
+            fatalError("Could not copy database")
+        }
+    }
+
+    func createIndex(_ database: Database) {
+        do {
+            try database.createIndex(IndexBuilder.fullTextIndex(items: FullTextIndexItem.property("description")).ignoreAccents(false), withName: "descFTSIndex")
+        } catch {
+            print(error)
+        }
+    }
+
+}
+// end::setup-database[]
