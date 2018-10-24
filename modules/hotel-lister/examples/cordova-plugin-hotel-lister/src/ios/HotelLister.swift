86e0a44583994cd030e024c007c83ff2d61fd136
James Nocentini <james.nocentini@gmail.com>
2018-10-24 15:49:38 +0200

    update plugin's HotelLister.swift

--- modules/hotel-lister/examples/cordova-plugin-hotel-lister/src/ios/HotelLister.swift
+++ modules/hotel-lister/examples/cordova-plugin-hotel-lister/src/ios/HotelLister.swift
@@ -1,3 +1,46 @@
-@objc(HotelLister) class HotelLister : CDVPlugin {
+import CouchbaseLiteSwift
 
+@objc(HotelLister) class HotelLister : CDVPlugin {
+    
+    var database: Database?
+    
+    // tag::initialize[]
+    override func pluginInitialize() {
+        self.database = DatabaseManager.sharedInstance().database
+    }
+    // end::initialize[]
+    
+    // tag::query-hotels[]
+    @objc(queryHotels:)
+    func queryHotels(command: CDVInvokedUrlCommand) {
+        let DOC_TYPE = "hotel";
+
+        let query = QueryBuilder
+            .select(
+                SelectResult.expression(Meta.id),
+                SelectResult.property("address"),
+                SelectResult.property("phone"),
+                SelectResult.property("name")
+            )
+            .from(DataSource.database(database!))
+            .where(
+                Expression.property("type")
+                    .equalTo(Expression.string(DOC_TYPE))
+        )
+        
+        do {
+            let resultSet = try query.execute()
+            let resultSetArray = resultSet.allResults()
+            var array = [Any]()
+            for item in resultSetArray {
+                array.append(item.toDictionary())
+            }
+            let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: array)
+            self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
+        } catch {
+            fatalError(error.localizedDescription);
+        }
+    }
+    // end::query-hotels[]
+    
 }
