aa2428efb05a358443e2f7a50dafffd6542b3055
James Nocentini <james.nocentini@gmail.com>
2018-11-01 14:51:11 +0100

    update upload.sh

--- modules/hotel-lister/examples/upload.sh
+++ modules/hotel-lister/examples/upload.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-s3cmd --acl-public -r put ../../../../docs-site/public/ s3://docs.couchbase.com/samples-js-staging/
+# staging
+# s3cmd --acl-public -r put ../../../../docs-site/public/ s3://docs.couchbase.com/samples-js-staging/
+# production
+# s3cmd --acl-public -r put ../../../../docs-site/public/tutorials/ s3://docs.couchbase.com/tutorials/
