3266acd72b49947b183df176fcbc8b2fefd5b64a
James Nocentini <james.nocentini@gmail.com>
2019-01-17 19:20:19 +0000

    delete upload.sh

--- modules/hotel-lister/examples/upload.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# staging
-# s3cmd --acl-public -r put ../../../../docs-site/public/ s3://docs.couchbase.com/samples-js-staging/
-# production
-# s3cmd --acl-public -r put ../../../../docs-site/public/tutorials/ s3://docs.couchbase.com/tutorials/
